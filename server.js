2cf6cd1104d28bc30d7eed93396fd89ab7a6e00c
liamdraper <liamdraper83@gmail.com>
2023-02-10 15:05:46 -0800

    Fix Google Oauth bug:

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 var passport = require('passport');
 var methodOverride = require('method-override');
 
@@ -31,6 +32,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
