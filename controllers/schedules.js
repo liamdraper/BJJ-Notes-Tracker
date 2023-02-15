df1d8ed56af6786955285b3fbeabee019d89901d
liamdraper <liamdraper83@gmail.com>
2023-02-15 13:51:09 -0800

    Add to Heroku

--- controllers/schedules.js
+++ controllers/schedules.js
@@ -8,36 +8,36 @@ module.exports = {
 }
 
 function index (req, res) {
-    Schedule.find({}, function(err, schedules) {
-        res.render('schedules/index', { schedules });
-    });
+  Schedule.find({}, function(err, schedules) {
+      res.render('schedules/index', { schedules });
+  });
 }
 
 function create (req, res) {
-    req.body.user = req.user._id;
-    const schedule = new Schedule(req.body);
-    schedule.save(function (err) {
-        req.body.user = req.user._id;
-        if (err) return res.redirect('/schedules');
-        res.redirect('/schedules');
-    })
+  req.body.user = req.user._id;
+  const schedule = new Schedule(req.body);
+  schedule.save(function (err) {
+      req.body.user = req.user._id;
+      if (err) return res.redirect('/schedules');
+      res.redirect('/schedules');
+  })
 }
 
 function deleteSchedule(req, res) {
-    Schedule.findOneAndDelete(
-      {_id: req.params.id}, function(err) {
-        res.redirect('/schedules');
-      }
-    );
-  }
+  Schedule.findOneAndDelete(
+    {_id: req.params.id}, function(err) {
+      res.redirect('/schedules');
+    }
+  );
+}
 
 function update(req, res) {
-    Schedule.findOneAndUpdate(
-      {_id: req.params.id},
-      req.body,
-      function(err, schedule) {
-        if (err || !schedule) return res.redirect('/schedules');
-        res.redirect('/schedules');
-      }
-    );
-  }
+  Schedule.findOneAndUpdate(
+    {_id: req.params.id},
+    req.body,
+    function(err, schedule) {
+      if (err || !schedule) return res.redirect('/schedules');
+      res.redirect('/schedules');
+    }
+  );
+}
